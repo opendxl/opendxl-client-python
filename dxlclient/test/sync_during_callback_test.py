c7e2f23626b812ab5ea2b59aaaece2117c19e0bd
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-03-01 10:05:08 -0800

    Fixes to sync_during_callback_test for Python 3 and better perf

--- dxlclient/test/sync_during_callback_test.py
+++ dxlclient/test/sync_during_callback_test.py
@@ -1,14 +1,18 @@
 from __future__ import absolute_import
+from threading import Condition
 import time
 from dxlclient import UuidGenerator, Request, EventCallback, Event
 from dxlclient.test.base_test import BaseClientTest
 from nose.plugins.attrib import attr
+import sys
 
 @attr('system')
 class TestSyncDuringCallback(BaseClientTest):
 
-    SLEEP_TIME = 5
-    exceptions = []
+    MAX_WAIT = 60
+    request_exception_message = None
+    event_received = False
+    event_received_condition = Condition()
 
     # Test to ensure that synchronous requests can't be made on
     # the incoming message thread
@@ -22,24 +26,27 @@ class TestSyncDuringCallback(BaseClientTest):
 
             # callback
             def event_callback(event):
-                try:
-                    req = Request(destination_topic=req_topic)
-                    client.sync_request(req)
-                except Exception as e:
-                    self.exceptions.append(e)
-                    raise e
+                with self.event_received_condition:
+                    self.event_received = True
+                    try:
+                        req = Request(destination_topic=req_topic)
+                        client.sync_request(req)
+                    except Exception as e:
+                        self.request_exception_message = str(e)
+                    self.event_received_condition.notify_all()
 
             ec = EventCallback()
             ec.on_event = event_callback
 
             client.add_event_callback(event_topic, ec)
 
-            time.sleep(self.SLEEP_TIME)  # Check the time
-
             evt = Event(destination_topic=event_topic)
             client.send_event(evt)
 
-            time.sleep(self.SLEEP_TIME)
-
-            self.assertTrue(self.exceptions[0] is not None)
-            self.assertTrue("different thread" in self.exceptions[0].message)
+            start = time.time()
+            with self.event_received_condition:
+                while (time.time() - start < self.MAX_WAIT) and \
+                        not self.event_received:
+                    self.event_received_condition.wait(self.MAX_WAIT)
+            self.assertIsNotNone(self.request_exception_message)
+            self.assertIn("different thread", self.request_exception_message)
