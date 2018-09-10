da7b6cbdcf8754cc4381626a254a194931686b77
Jeremy Barlow <jeremy_barlow@mcafee.com>
2018-09-10 16:42:08 -0700

    Add Python 3.7 to .travis.yml and setup.py

--- setup.py
+++ setup.py
@@ -126,7 +126,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
 
     cmdclass={
